837cb6209f22b62e9c74b23664b0d47ccd773aa5
pengyu <pengyufreedom@aliyun.com>
2017-08-29 23:49:25 +0800

    ..

--- core/src/main/java/mysso/ticket/registry/TicketRegistry.java
+++ core/src/main/java/mysso/ticket/registry/TicketRegistry.java
@@ -9,16 +9,24 @@ import java.util.Map;
  */
 public interface TicketRegistry {
     /**
-     * add the given ticket to the registry.
-     * if the ticket.id already exists in the registry, then a TicketException would be thrown.
+     * 添加一个 ticket
+     * id 不可重复，如果 id 重复，会抛出一个 DuplicateIdException 异常
+     *
      * @param ticket
      */
     void add(AbstractTicket ticket);
 
+    /**
+     * 更新 ticket
+     * id 必须存在，如果 id 不存在，则会抛出一个 TicketIdNotExistsException 异常
+     * @param ticket
+     */
     void update(AbstractTicket ticket);
 
     /**
-     * delete the ticket by the given id from the registry
+     * 销毁、删除给定的 ticket
+     * 如果给定的是 TicketGrantingTicket类型，则删除其所关联的所有 service ticket 和 token
+     * 如果给定的是 Service Ticket 或 Token 类型，则在他们所关联的 TicketGrantingTicket 中的引用字段中删除
      * @param id
      * @param clazz supports TicketGrantingTicket, ServiceTicket, Token
      * @return
@@ -26,7 +34,8 @@ public interface TicketRegistry {
     boolean delete(String id, Class<? extends AbstractTicket> clazz);
 
     /**
-     * get the ticket by the given id
+     * 根据 id 获取指定类型的 ticket
+     *
      * @param id
      * @param clazz supports TicketGrantingTicket, ServiceTicket, Token
      * @param <T>
@@ -34,5 +43,11 @@ public interface TicketRegistry {
      */
     <T extends AbstractTicket> T get(String id, Class<T> clazz);
 
+    /**
+     * 获取指定类型全部的 ticket
+     * @param clazz
+     * @param <T>
+     * @return
+     */
     <T extends AbstractTicket> Map<String, T> getAll(Class<T> clazz);
 }
--- supports-redis/src/main/java/mysso/supports/redis/ticket/registry/RedisTicketRegistry.java
+++ supports-redis/src/main/java/mysso/supports/redis/ticket/registry/RedisTicketRegistry.java
@@ -1,66 +1,96 @@
-package mysso.ticket.registry;
+package mysso.supports.redis.ticket.registry;
 
 import mysso.ticket.*;
 import mysso.ticket.exception.DuplicateIdException;
 import mysso.ticket.exception.TicketIdNotExistsException;
 import mysso.ticket.exception.UnsupportTicketTypeException;
+import mysso.ticket.registry.TicketRegistry;
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.util.Assert;
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by pengyu on 2017/8/8.
+ * Created by pengyu on 17-8-29.
  */
-public class InMemoryTicketRegistry implements TicketRegistry {
-    private Map<String, TicketGrantingTicket> tgtMap = new HashMap<>();
-    private Map<String, ServiceTicket> stMap = new HashMap<>();
-    private Map<String, Token> tkMap = new HashMap<>();
+public class RedisTicketRegistry implements TicketRegistry {
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private Jedis jedis;
+    private String tgtPrefix = "tgt:";
+    private String stPrefix = "st:";
+    private String tkPrefix = "tk:";
 
     @Override
     public void add(AbstractTicket ticket) {
-        Assert.notNull(ticket, "ticket should not be null");
-        Map ticketMap;
+        Validate.notNull(ticket, "ticket should not be null");
+        String ticketPrefix = null;
         if (ticket instanceof TicketGrantingTicket) {
-            ticketMap = tgtMap;
+            ticketPrefix = tgtPrefix;
         } else if (ticket instanceof ServiceTicket) {
-            ticketMap = stMap;
+            ticketPrefix = stPrefix;
         } else if (ticket instanceof Token) {
-            ticketMap = tkMap;
+            ticketPrefix = tkPrefix;
         } else {
             throw new UnsupportTicketTypeException(String.format("the ticket type is unsupported: %s", ticket.getClass()));
         }
-        if (ticketMap.containsKey(ticket.getId())) {
-            throw new DuplicateIdException(String.format("the id of the ticket to be added has already exists: %s, ticket type: %s", ticket.getId(), ticket.getClass().getSimpleName()));
+        Boolean exists = jedis.exists(ticketPrefix + ticket.getId());
+        if (exists) {
+            log.warn("the id of the ticket to be added has already exists: {}, ticket type: {}",
+                    ticket.getId(), ticket.getClass().getSimpleName());
+            throw new DuplicateIdException(String.format("the id of the ticket to be added has already exists: " +
+                    "%s, ticket type: %s", ticket.getId(), ticket.getClass().getSimpleName()));
         }
-        ticketMap.put(ticket.getId(), ticket);
+        try {
+            final Map<String, String> describe = BeanUtils.describe(ticket);
+            log.trace("adding ticket to redis, type: {}, id: {}", ticket.getClass().getSimpleName(), ticket.getId());
+            jedis.hmset(ticketPrefix + ticket.getId(), describe);
+            log.info("added ticket to redis, type: {}, id: {}", ticket.getClass().getSimpleName(), ticket.getId());
+        } catch (Exception e) {
+            log.error("an exception occurred when adding a ticket, caused by: " + e.getMessage(), e);
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void update(AbstractTicket ticket) {
-        Assert.notNull(ticket, "ticket should not be null");
-        Map ticketMap;
+        Validate.notNull(ticket, "ticket should not be null");
+        String ticketPrefix = null;
         if (ticket instanceof TicketGrantingTicket) {
-            ticketMap = tgtMap;
+            ticketPrefix = tgtPrefix;
         } else if (ticket instanceof ServiceTicket) {
-            ticketMap = stMap;
+            ticketPrefix = stPrefix;
         } else if (ticket instanceof Token) {
-            ticketMap = tkMap;
+            ticketPrefix = tkPrefix;
         } else {
             throw new UnsupportTicketTypeException(String.format("the ticket type is unsupported: %s", ticket.getClass()));
         }
-        if (!ticketMap.containsKey(ticket.getId())) {
+        Boolean exists = jedis.exists(ticketPrefix + ticket.getId());
+        if (!exists) {
+            log.warn("the id of the ticket to be updated does not exist, it might be deleted from the registry: " +
+                    "{}, ticket type: {}", ticket.getId(), ticket.getClass().getSimpleName());
             throw new TicketIdNotExistsException(String.format("the id of the ticket to be updated does not exist, " +
                     "it might be deleted from the registry: %s, ticket type: %s", ticket.getId(), ticket.getClass().getSimpleName()));
         }
-        ticketMap.put(ticket.getId(), ticket);
+        try {
+            log.trace("updating ticket to redis, type: {}, id: {}", ticket.getClass().getSimpleName(), ticket.getId());
+            final Map<String, String> describe = BeanUtils.describe(ticket);
+            jedis.hmset(ticketPrefix + ticket.getId(), describe);
+            log.info("updated ticket to redis, type: {}, id: {}", ticket.getClass().getSimpleName(), ticket.getId());
+        } catch (Exception e) {
+            log.error("an exception occurred when adding a ticket, caused by: " + e.getMessage(), e);
+            e.printStackTrace();
+        }
     }
 
     @Override
     public boolean delete(String id, Class<? extends AbstractTicket> clazz) {
-        Assert.notNull(id, "id should not be null");
-        Assert.notNull(clazz, "clazz should not be null");
+        Validate.notNull(id, "id should not be null");
+        Validate.notNull(clazz, "clazz should not be null");
         if (StringUtils.equals(TicketGrantingTicket.class.getCanonicalName(), clazz.getCanonicalName())) {
             deleteChildren(tgtMap.get(id));
             if (tgtMap.remove(id) != null) {
@@ -83,7 +113,6 @@ public class InMemoryTicketRegistry implements TicketRegistry {
         }
         return false;
     }
-
     private void deleteChildren(TicketGrantingTicket tgt) {
         if (tgt == null) {
             return;
@@ -153,5 +182,37 @@ public class InMemoryTicketRegistry implements TicketRegistry {
             throw new UnsupportTicketTypeException(String.format("the ticket type is unsupported: %s", clazz));
         }
         return new HashMap<>(ticketMap);
+    }
+
+    public Jedis getJedis() {
+        return jedis;
+    }
+
+    public void setJedis(Jedis jedis) {
+        this.jedis = jedis;
+    }
+
+    public String getTgtPrefix() {
+        return tgtPrefix;
+    }
+
+    public void setTgtPrefix(String tgtPrefix) {
+        this.tgtPrefix = tgtPrefix;
+    }
+
+    public String getStPrefix() {
+        return stPrefix;
+    }
+
+    public void setStPrefix(String stPrefix) {
+        this.stPrefix = stPrefix;
+    }
+
+    public String getTkPrefix() {
+        return tkPrefix;
+    }
+
+    public void setTkPrefix(String tkPrefix) {
+        this.tkPrefix = tkPrefix;
     }
 }
