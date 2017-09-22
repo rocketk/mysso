9cb309cb69a794be01b612ec303a90dd609ba432
pengyu <pengy70@chinaunicom.cn>
2017-09-22 17:52:14 +0800

    ..

---
+++ supports-jdbc/src/main/java/mysso/supports/jdbc/serviceprovider/registry/JdbcServiceProviderRegistry.java
@@ -0,0 +1,31 @@
+package mysso.supports.jdbc.serviceprovider.registry;
+
+import mysso.serviceprovider.ServiceProvider;
+import mysso.serviceprovider.registry.ServiceProviderRegistry;
+
+import java.util.Map;
+
+/**
+ * Created by pengyu
+ */
+public class JdbcServiceProviderRegistry implements ServiceProviderRegistry{
+    @Override
+    public ServiceProvider get(String spId) {
+        return null;
+    }
+
+    @Override
+    public void save(ServiceProvider sp) {
+
+    }
+
+    @Override
+    public boolean delete(ServiceProvider sp) {
+        return false;
+    }
+
+    @Override
+    public Map<String, ServiceProvider> getAll() {
+        return null;
+    }
+}
